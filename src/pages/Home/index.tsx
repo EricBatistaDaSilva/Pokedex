6874d1a64766e64872ee48f6ba456f6d6a0d24fe
Eric Batista <batistaeric62@gmail.com>
2024-01-05 21:25:49 -0300

    add use query pokemoon page and card pokemon

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,20 @@
-import axios from "axios";
+import { PokemonCard } from "../../components/PokemonCard";
+import { useQueryPokemonPage } from "../../hooks/useQueryPokemonPage";
 import { Container } from "./styles";
 
 export function Home() {
-  const {data} = axios.get("/pokemon");
+  const { data } = useQueryPokemonPage();
   console.log(data);
 
   return (
     <Container>
-      <h1>Home</h1>
+      <h1>{"Bem vindo(a) à sua nova pokédex"}</h1>
+
+      <div className="gridCards">
+        {data?.map((pokemon) => {
+          return <PokemonCard pokemon={pokemon}/>;
+        })}
+      </div>
     </Container>
   );
 }
