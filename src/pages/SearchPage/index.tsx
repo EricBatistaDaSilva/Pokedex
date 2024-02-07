1e6ff85aad4911bd4be67a0fc5d8bb42c79ff093
Eric Batista <batistaeric62@gmail.com>
2024-02-06 21:35:25 -0300

    add page search

--- src/pages/SearchPage/index.tsx
+++ src/pages/SearchPage/index.tsx
@@ -1,9 +1,35 @@
+import { Link, useSearchParams } from "react-router-dom";
 import { Container } from "./style";
+import { useQueryPokemonFiltered } from "../../hooks/useQueryPokemonFiltered";
+import { PokemonCard } from "../../components/PokemonCard";
 
 export function SearchPage() {
+  const searchParams = useSearchParams();
+  const queryPokemonName = searchParams[0].get("q");
+  const { data, isLoading, error } = useQueryPokemonFiltered(queryPokemonName!);
+
+  if (error) console.error(error);
+
   return (
     <Container>
-      <h1>SearchPage</h1>
+      {isLoading && <span className="feedbackLoading">Buscando...</span>}
+      {!isLoading && error && <span className="feedbackLoading">Error...</span>}
+
+      {data && (
+        <>
+          <h1>{`${data.length} dado(s) encontrados sobre o pokemon "${queryPokemonName}"`}</h1>
+
+          <div className="gridCards">
+            {data.map((pokemon) => {
+              return (
+                <Link to={`/details/${pokemon.name}`} key={pokemon.id}>
+                  <PokemonCard pokemon={pokemon} />
+                </Link>
+              );
+            })}
+          </div>
+        </>
+      )}
     </Container>
   );
 }
